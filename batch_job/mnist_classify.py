264e0c272fbcd99523f60ca0d9b7520a13f6ad16
Jonathan Halverson <52128661+jdh4@users.noreply.github.com>
2025-10-09 16:16:06 -0400

    Update mnist_classify.py

--- batch_job/mnist_classify.py
+++ batch_job/mnist_classify.py
@@ -122,9 +122,9 @@ def main():
         v2.ToTensor(),
         v2.Normalize((0.1307,), (0.3081,))
         ])
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
+    dataset1 = datasets.MNIST('./data', train=True, download=False,
                        transform=transform)
-    dataset2 = datasets.MNIST('../data', train=False,
+    dataset2 = datasets.MNIST('./data', train=False, download=False,
                        transform=transform)
     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
